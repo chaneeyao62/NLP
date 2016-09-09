661b90da80d28d2d2b0a78e08bb300ea527aed34
Rae <ruichen@Ruis-MacBook-Air.local>
2016-09-09 13:21:55 -0400

    I add for loop to construct the whole database for a topic.
    Strange; There is no file 171. I am not sure if I can eliminate 171 via
    pass comment?? Maybe should use try catch.

--- p1.py
+++ p1.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from __future__ import division
 import nltk, re, pprint
 from nltk import word_tokenize
@@ -11,16 +12,51 @@ with open('data_corrected/classification task/autos/train_docs/autos_file0.txt',
 
 sentence = "At eight o'clock on Thursday morning Arthur didn't feel very very good. on Monday morning Arthur feels better"
 tokens = nltk.word_tokenize(sentence)
+=======
+a = 101
+
+import nltk, re, pprint
+from nltk import word_tokenize
+>>>>>>> Stashed changes
 
 #Building Dictionary
 dictionary = {}
 
+
+for i in range(0,300):
+    if i == 171:
+        pass
+    else:
+       filename = 'data_corrected/classification task/autos/train_docs/autos_file'+str(i)+'.txt'
+       with open(filename,'r') as myfile:
+           file = myfile.read()
+       tokens = nltk.word_tokenize(file)
+    
+       for index in range(len(tokens)):
+          if tokens[index] not in dictionary:
+              dictionary.update({tokens[index]:1})
+          else:
+              dictionary[tokens[index]] = dictionary[tokens[index]]+1
+
+print dictionary
+
+
+
+
+
+"""with open('data_corrected/classification task/autos/train_docs/autos_file0.txt','r') as myfile:
+    file = myfile.read()
+#file = open('data_corrected/classification task/autos/train_docs/autos_file0.txt','r')
+
+sentence = "At eight o'clock on Thursday morning Arthur didn't feel very very good. on Monday morning Arthur feels better"
+tokens = nltk.word_tokenize(file)
 for index in range(len(tokens)):
     if tokens[index] not in dictionary:
         dictionary.update({tokens[index]:1})
     else:
         dictionary[tokens[index]] = dictionary[tokens[index]]+1
     
+<<<<<<< Updated upstream
 
 
 #Random Generator
@@ -30,3 +66,7 @@ valSum = sum(dictionary.values())
 prob = [x / valSum for x in values]
 
 random1 = numpy.random.choice(dictionary.keys(), p = prob)
+=======
+print dictionary"""
+    
+>>>>>>> Stashed changes
