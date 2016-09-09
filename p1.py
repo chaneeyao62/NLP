1e19faf1f816ac154a3cc064513fd591975554a8
Qianyan Yao <yaoqianyan@gmail.com>
2016-09-09 13:11:27 -0400

    Random Generator
    add package and rand. gen

--- p1.py
+++ p1.py
@@ -1,13 +1,18 @@
-a = 101
+from __future__ import division
+import nltk, re, pprint
+from nltk import word_tokenize
+import numpy
+
+
 with open('data_corrected/classification task/autos/train_docs/autos_file0.txt','r') as myfile:
     file = myfile.read()
 #file = open('data_corrected/classification task/autos/train_docs/autos_file0.txt','r')
 
-import nltk, re, pprint
-from nltk import word_tokenize
-sentence = "At eight o'clock on Thursday morning Arthur didn't feel very very good. on Monday morning Arthur feels better"
-tokens = nltk.word_tokenize(file)
 
+sentence = "At eight o'clock on Thursday morning Arthur didn't feel very very good. on Monday morning Arthur feels better"
+tokens = nltk.word_tokenize(sentence)
+
+#Building Dictionary
 dictionary = {}
 
 for index in range(len(tokens)):
@@ -16,5 +21,12 @@ for index in range(len(tokens)):
     else:
         dictionary[tokens[index]] = dictionary[tokens[index]]+1
     
-    #ggggg
-    
+
+
+#Random Generator
+test = numpy.random.choice(["a","b","c"], p=[0.2,0.3,0.5])
+values = dictionary.values()
+valSum = sum(dictionary.values())
+prob = [x / valSum for x in values]
+
+random1 = numpy.random.choice(dictionary.keys(), p = prob)
