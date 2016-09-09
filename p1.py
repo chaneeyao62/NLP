2fb82921a7e4325e13ed88a7c4a3f0ff8f219efc
Qianyan Yao <yaoqianyan@gmail.com>
2016-09-09 17:33:35 -0400

    Removed Unnecessary code

--- p1.py
+++ p1.py
@@ -1,18 +1,12 @@
 from __future__ import division
-import nltk, re, pprint
-from nltk import word_tokenize
 import numpy
 
 
 with open('data_corrected/classification task/autos/train_docs/autos_file0.txt','r') as myfile:
     file = myfile.read()
-#file = open('data_corrected/classification task/autos/train_docs/autos_file0.txt','r')
 
-import nltk, re, pprint
-from nltk import word_tokenize
 
 sentence = "At eight o'clock on Thursday morning Arthur didn't feel very very good . on Monday morning Arthur feels better"
-#tokens = nltk.word_tokenize(file)
 tokens = file.split()
 beginIndex = tokens.index('Subject')+2
 tokens = tokens[beginIndex:]
@@ -38,12 +32,6 @@ dictionary = {}
               dictionary.update({tokens[index]:1})
           else:
               dictionary[tokens[index]] = dictionary[tokens[index]]+1"""
-
-with open('data_corrected/classification task/autos/train_docs/autos_file0.txt','r') as myfile:
-    file = myfile.read()
-#file = open('data_corrected/classification task/autos/train_docs/autos_file0.txt','r')
-
-sentence = "At eight o'clock on Thursday morning Arthur didn't feel very very good. on Monday morning Arthur feels better"
 
 for index in range(len(tokens)):
     if tokens[index] not in dictionary:
