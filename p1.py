0e8ced14ef8cfd500ed91c345e1d1e8ab4c25012
Qianyan Yao <yaoqianyan@gmail.com>
2016-09-09 21:30:08 -0400

    bigram

--- p1.py
+++ p1.py
@@ -48,3 +48,21 @@ random1 = numpy.random.choice(dictionary.keys(), p = prob)
 random2 = numpy.random.choice(dictionary.keys(), p = prob)
 random3 = numpy.random.choice(dictionary.keys(), p = prob)
 
+#Bigram
+dictionary2 = {}
+for index in range(len(tokens)-1):
+    if tokens[index] not in dictionary2:
+        dictionary2.update({tokens[index]:{tokens[index+1]:1}})
+    else:
+        if tokens[index+1] not in dictionary2[tokens[index]]:
+            dictionary2[tokens[index]].update({tokens[index+1]:1})
+        else:
+            dictionary2[tokens[index]][tokens[index+1]] = dictionary2[tokens[index]][tokens[index+1]]+1
+
+if tokens[len(tokens)-1] in dictionary2:
+    dictionary2[tokens[len(tokens)-1]].update({'<s>':1})
+else:
+    dictionary2.update({tokens[len(tokens)-1]:{'<s>':1}})
+    
+
+
